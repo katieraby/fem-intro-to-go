55261f1c5442be69db496800bca26e1478d9402b
Katie <50029859+katieraby@users.noreply.github.com>
2021-01-05 17:26:13 +0000

    Variadic funcs exercise 4a & b

--- 04_complex_structures/code/exercise_4a.go
+++ 04_complex_structures/code/exercise_4a.go
@@ -1,10 +1,10 @@
 package main
-
-func average (arg1, arg2, arg3 float64) (averageOfNums float64) {
-	averageOfNums = (arg1 + arg2 + arg3) / 3
-	return
-}
-
-func main() {
-	average(6.1, 1.2, 3.3)
-}
+//
+//func average (arg1, arg2, arg3 float64) (averageOfNums float64) {
+//	averageOfNums = (arg1 + arg2 + arg3) / 3
+//	return
+//}
+//
+//func main() {
+//	average(6.1, 1.2, 3.3)
+//}
---
+++ 04_complex_structures/code/exercise_4b.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func average (args ...float64) (averageOfNums float64) {
+	var totalOfNums float64
+	fmt.Println(totalOfNums)
+	for _, value := range args {
+		totalOfNums += value
+	}
+	averageOfNums = totalOfNums / float64(len(args))
+	return
+}
+
+func main() {
+	average(6.1, 1.2, 3.3, 4.5, 8.6)
+}
