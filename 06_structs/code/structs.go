1a57ddd94d51ab1a29d348a799e4cef0fb757036
Katie <50029859+katieraby@users.noreply.github.com>
2021-01-15 21:05:42 +0000

    Structs exercise

--- 06_structs/code/structs.go
+++ 06_structs/code/structs.go
@@ -4,18 +4,42 @@ import "fmt"
 
 // User is a user type
 type User struct {
-	ID                         int
-	FirstName, LastName, Email string
+ID					       int
+FirstName, LastName, Email string
 }
 
-// // User is a user type
-// type User struct {
-// ID					       int
-// FirstName, LastName, Email string
-// }
+// Group is a group type
+type Group struct {
+	role            string
+	users           []User
+	newestUser      User
+	spaceAvailable  bool
+}
+
+func describeUser(u User) string {
+	desc := fmt.Sprintf("Name: %s %s, Email: %s, ID: %d", u.FirstName, u.LastName, u.Email, u.ID)
+	return desc
+}
+
+func describeGroup(g Group) string {
+	if len(g.users) >= 2 {
+		g.spaceAvailable = false
+	}
+	desc := fmt.Sprintf("The %s user group has %d users. Newest user:  %s, Accepting New Users:  %t", g.role, len(g.users), g.newestUser.FirstName, g.spaceAvailable)
+	return desc
+}
 
 func main() {
-	u := User{ID: 1, FirstName: "Marilyn", LastName: "Monroe", Email: "marilyn.monroe@gmail.com"}
+	u1 := User{ID: 1, FirstName: "Marilyn", LastName: "Monroe", Email: "marilyn.monroe@gmail.com"}
+	u2 := User{ID: 2, FirstName: "Humphrey", LastName: "Bogart", Email: "humphrey.bogart@gmail.com"}
 
-	fmt.Println(u)
+	g := Group{role: "admin", users: []User{u1, u2}, newestUser: u2, spaceAvailable: true}
+
+	userDescription := describeUser(u1)
+	groupDescription := describeGroup(g)
+
+	fmt.Println(userDescription)
+	fmt.Println(groupDescription)
+
+	fmt.Println(g)
 }
