96fa53fee8c252c725d3f454736b1aa2d2f35d53
Katie Raby <katie.raby@icloud.com>
2021-01-18 17:11:34 +0000

    Todo list server with html template

--- 11_server/code/exercise_11a.go
+++ 11_server/code/exercise_11a.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -10,9 +11,57 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home!")
 }
 
+// Todo is a todo with a title and content
+type Todo struct {
+	Title string
+	Content string
+}
+
+//PageVariables are variables sent to html template
+type PageVariables struct {
+	PageTitle string
+	PageTodos []Todo
+}
+
+var todos []Todo
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	pageVariables := PageVariables{
+		PageTitle: "Get Todos",
+		PageTodos:  todos,
+	}
+
+	t, err := template.ParseFiles("todos.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Template parsing error: ", err)
+	}
+
+	err = t.Execute(w, pageVariables)
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Request parsing error: ", err)
+	}
+
+	todo := Todo{
+		Title: r.FormValue("title"),
+		Content: r.FormValue("content"),
+	}
+
+	todos = append(todos, todo)
+	log.Print(todos)
+	http.Redirect(w, r, "/todos/", http.StatusSeeOther)
+}
+
 func main() {
 	http.HandleFunc("/", home)
-	fmt.Println("Building a web server")
+	http.HandleFunc("/todos/", getTodos)
+	http.HandleFunc("/add-todo/", addTodo)
 	// even wrapped in log fatal, the code will still execute
 	// will fallback to log fatal if there is an error
 	log.Fatal(http.ListenAndServe(":8080", nil))
